f3883abd7c8a84d4438c5e1368c9d54f3a8d728a
“ogunniranRuth” <iyaboogunniran071316@yahoo.com>
2022-12-07 07:05:39 -0800

    task 1022

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,9 +4,11 @@ def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
     if a < b:
+
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
             return (c)
         else:
+            
             return(sub(a, b))
